86395134e2b8a74cec7adf3cc54613fe9551f97f
Daniel Haase <git@dhaase.de>
2015-05-11 01:42:34 +0200

    initial commit, initialized R module

---
+++ dh/R.py
@@ -0,0 +1,13 @@
+def unique(x):
+    y = []
+    for item in x:
+        if item not in y:
+            yield item
+            y.append(item)
+
+def which(x):
+    index = 0
+    for item in x:
+        if item:
+            yield index
+        index += 1
