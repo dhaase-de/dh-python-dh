c1a4ef2b02bf75a2989390be694b9a3c81fc16df
Daniel Haase <git@dhaase.de>
2015-08-30 21:43:06 +0200

    added installer (distutils -> setup.py)

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# robust bash scripting
+set -o errexit
+set -o nounset
+
+# get absolute path of this script
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+"$SCRIPT_DIR"/setup.py install --user
+
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import os.path
+import re
+
+from distutils.core import setup
+
+##
+## preparations
+##
+
+# name of the top-level package - must match top level directory name
+packageName = "dh"
+
+# parse version from package's init file
+version = None
+versionFilename = os.path.join(os.path.dirname(os.path.realpath(__file__)), packageName, "__init__.py")
+print(versionFilename)
+with open(versionFilename, "r") as f:
+    for line in f:
+        versionMatch = re.search("^\s*__version__\s*=\s*\"([0-9]+\.[0-9]+\.[0-9]+)\"", line)
+        if versionMatch is not None:
+            version = versionMatch.group(1)
+            break
+if version is None:
+    raise RuntimeError("Could not parse version from file '{}'".format(versionFilename))
+
+##
+## main call
+##
+
+setup(
+    name = packageName,
+    version = version,
+    description = "Personal package of Daniel Haase",
+    author = "Daniel Haase",
+    packages = [os.path.join(os.path.dirname(os.path.realpath(__file__)), packageName)],
+)
+
