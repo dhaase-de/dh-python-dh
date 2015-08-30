d01392b3355a7bb8d19d752391da8e79e01ecaa5
Daniel Haase <git@dhaase.de>
2015-08-31 00:21:34 +0200

    fixed setup.py not installing thirdparty modules

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import os.path
 import re
 
@@ -9,12 +10,13 @@ from distutils.core import setup
 ## preparations
 ##
 
-# name of the top-level package - must match top level directory name
-packageName = "dh"
+# (relative) directories of the top-level packages
+packageDirBase = "dh"
+packageDirThirdparty = os.path.join(packageDirBase, "thirdparty")
 
 # parse version from package's init file
 version = None
-versionFilename = os.path.join(packageName, "__init__.py")
+versionFilename = os.path.join(packageDirBase, "__init__.py")
 print(versionFilename)
 with open(versionFilename, "r") as f:
     for line in f:
@@ -25,15 +27,22 @@ with open(versionFilename, "r") as f:
 if version is None:
     raise RuntimeError("Could not parse version from file '{}'".format(versionFilename))
 
+# prepare package list (list is: base package plus "thirdparty" module plus one package for each third party module)
+packages = [packageDirBase, packageDirThirdparty]
+for (path, packageSubdirsThirdparty, _) in os.walk(packageDirThirdparty):
+    for packageSubdirThirdparty in packageSubdirsThirdparty:
+        if re.search("^[^_]", packageSubdirThirdparty):
+            packages.append(os.path.join(path, packageSubdirThirdparty))
+
 ##
 ## main call
 ##
 
 setup(
-    name = packageName,
+    name = packageDirBase,
     version = version,
     description = "Personal package of Daniel Haase",
     author = "Daniel Haase",
-    packages = [packageName],
+    packages = packages,
 )
 
