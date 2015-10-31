05f15e8b2483469adafa3dd67f6af31be1cc2fca
Daniel Haase <git@dhaase.de>
2015-10-31 10:01:11 +0100

    updated build framework

--- clean.sh
+++ clean.sh
@@ -7,5 +7,11 @@ set -o nounset
 # get absolute path of this script
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
+# clean build output
 "$SCRIPT_DIR"/setup.py clean --all
+
+# clean bytecode
+find "$SCRIPT_DIR" -path '*/__pycache__/*.pyc' -type f -delete
+find "$SCRIPT_DIR" -name '__pycache__' -type d -delete
+find "$SCRIPT_DIR" -name '*.pyc' -type f -delete
 
