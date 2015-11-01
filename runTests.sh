8f684a7bcc5bf3941022aa98585ebdc44b69a33e
Daniel Haase <git@dhaase.de>
2015-11-01 18:19:42 +0100

    increased unit test verbosity

--- runTests.sh
+++ runTests.sh
@@ -7,4 +7,4 @@ set -o nounset
 # get absolute path of this script
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-cd "$SCRIPT_DIR" && nosetests3 dh --with-doctest
+cd "$SCRIPT_DIR" && nosetests3 dh --with-doctest --verbosity=3
