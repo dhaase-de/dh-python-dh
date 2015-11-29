79ac801293b5b6a51e563ddfe357fae6c15c214f
Daniel Haase <git@dhaase.de>
2015-11-29 14:40:54 +0100

    fixed install routine using old install script location

--- dev/test.py
+++ dev/test.py
@@ -2,18 +2,17 @@
 
 import os
 import subprocess
-import time
+
 
 def install():
     print("Installing package 'dh'...")
     workingDir = os.getcwd()
     scriptDir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(scriptDir + "/..")
-    subprocess.call(["./install.sh"], stdout = open(os.devnull, 'wb'))
+    subprocess.call(["./scripts/build-install.sh"], stdout = open(os.devnull, 'wb'))
     os.chdir(workingDir)
     print("done")
     print("-" * 26)
 
 if __name__ == "__main__":
     install()
-
