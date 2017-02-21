b86b856864cca88e63e4628a6f4cf6cce5fd95d7
Daniel Haase <git@dhaase.de>
2017-02-21 01:19:49 +0100

    [feature] added NumpySocketMessage, nodelay option for SocketServer/Client, and added ImageProcessingServer/Client

--- dh/network/__init__.py
+++ dh/network/__init__.py
@@ -8,12 +8,21 @@ import json
 import socket
 import struct
 import sys
+import time
 import zlib
 
 import dh.utils
 
 import dh.data
 import dh.image
+
+# NumPy is only needed for some parts and is optional
+try:
+    import numpy as np
+except ImportError as e:
+    _NUMPY_ERROR = e
+else:
+    _NUMPY_ERROR = None
 
 
 ###
@@ -94,6 +103,27 @@ class ByteSocketMessage(SocketMessage):
         return b
 
 
+class NumpySocketMessage(ByteSocketMessage):
+    """
+    Class providing `send()` and `recv()` methods for sending and receiving
+    NumPy ndarray objects via the given socket.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if _NUMPY_ERROR is not None:
+            raise _NUMPY_ERROR
+        super().__init__(*args, **kwargs)
+
+    def send(self, x):
+        b = io.BytesIO()
+        np.save(file=b, arr=x, allow_pickle=False, fix_imports=False)
+        super().send(b.getvalue())
+
+    def recv(self):
+        b = io.BytesIO(super().recv())
+        return np.load(file=b, allow_pickle=False, fix_imports=False)
+
+
 class JsonSocketMessage(ByteSocketMessage):
     """
     Class providing `send()` and `recv()` methods for sending and receiving
@@ -138,25 +168,40 @@ class SocketServer(abc.ABC):
     Simple socket server which accepts connections on the specified `host`
     and `port` and communicates using the specified message type `messageClass`
     in the method `handler`.
+
+    See http://stackoverflow.com/a/19742674/1913780 for an explanation of
+    `nodelay`.
     """
 
-    def __init__(self, host="", port=7214, backlog=5, messageClass=ByteSocketMessage):
+    def __init__(self, host="", port=7214, backlog=5, nodelay=True, messageClass=ByteSocketMessage):
         print("Creating socket...")
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("Binding socket to {}:{}...".format(host, port))
+        if nodelay:
+            self._socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+        print("Binding socket to {}:{}...".format(host if len(host) > 0 else "*", port))
         self._socket.bind((host, port))
         self._backlog = backlog
+        self._nodelay = nodelay
         self._messageClass = messageClass
+
+    def _print(self, text):
+        print("[{}]  {}".format(dh.utils.dtstr(compact=False), text))
 
     def run(self):
         self._socket.listen(self._backlog)
         while True:
-            print("Waiting for connection...")
+            self._print("Waiting for connection...")
             sys.stdout.flush()
             (connectionSocket, connectionAddress) = self._socket.accept()
-            print("Accepted connection from {}:{}".format(connectionAddress[0], connectionAddress[1]))
+            self._print("Accepted connection from {}:{}".format(connectionAddress[0], connectionAddress[1]))
+            t0 = time.time()
             communication = self._messageClass(connectionSocket)
-            self.handler(communication=communication)
+            try:
+                self.handler(communication=communication)
+            except Exception as e:
+                self._print("** {}: {}".format(type(e).__name__, e))
+            self._print("Finished request from {}:{} after {} ms".format(connectionAddress[0], connectionAddress[1], dh.utils.around((time.time() - t0) * 1000.0)))
 
     @abc.abstractmethod
     def handler(self, communication):
@@ -172,16 +217,22 @@ class SocketClient(abc.ABC):
     Simple socket client which connects to the server on the specified `host`
     and `port` each time `query()` is called. It communicates using the
     specified message type `messageClass` in the method `handler`.
+
+    See http://stackoverflow.com/a/19742674/1913780 for an explanation of
+    `nodelay`.
     """
 
-    def __init__(self, host, port=7214, messageClass=ByteSocketMessage):
+    def __init__(self, host, port=7214, nodelay=True, messageClass=ByteSocketMessage):
         self._host = host
         self._port = port
+        self._nodelay = nodelay
         self._messageClass = messageClass
 
     def query(self, *args, **kwargs):
         # establish connection with the server
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if self._nodelay:
+            self._socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         self._socket.connect((self._host, self._port))
 
         # user-specific communication
@@ -256,3 +307,65 @@ class DataProcessingClient(SocketClient):
         if res["status"] != "OK":
             raise RuntimeError("Received non-OK result status: ''".format(res["status"]))
         return res["result"]
+
+
+class ImageProcessingServer(SocketServer):
+    """
+    Special case of `SocketServer` which accepts a NumPy array and JSON-encoded
+    parameters and returns a NumPy array. The counterpart is the
+    `ImageProcessingClient` class.
+
+    To specify the processing behavior, sub-class this class and implement
+    the static method `process(data, params)`.
+
+    In contrast to `DataProcessingServer`, this class is less flexible but has
+    a lower response latency.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs["messageClass"] = NumpySocketMessage
+        super().__init__(*args, **kwargs)
+
+    def handler(self, communication):
+        data = communication.recv()
+        params = JsonSocketMessage(communication.socket).recv()
+        try:
+            result = self.process(data=data, params=params)
+        except Exception as e:
+            self._print("** {}: {}".format(type(e).__name__, e))
+            communication.send(np.zeros(shape=(0, 0), dtype="uint8"))
+        else:
+            communication.send(result)
+
+    @staticmethod
+    @abc.abstractmethod
+    def process(data, params):
+        pass
+
+
+class ImageProcessingClient(SocketClient):
+    """
+    Special case of `SocketClient` which sends a NumPy array and JSON-encoded
+    parameters and receives a NumPy array. The counterpart is the
+    `ImageProcessingServer` class.
+
+    The processing behavior is specified by sub-classing
+    `ImageProcessingServer` and implementing the static method
+    `process(data, params)`.
+
+    In contrast to `DataProcessingClient`, this class is less flexible but has
+    a lower response latency.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs["messageClass"] = NumpySocketMessage
+        super().__init__(*args, **kwargs)
+
+    def process(self, data, params):
+        return self.query(data=data, params=params)
+
+    def handler(self, data, params, communication):
+        communication.send(data)
+        JsonSocketMessage(communication.socket).send(params)
+        res = communication.recv()
+        return res
