6315eb85effbde254cabd6bc0e0555da9f9c7a38
Daniel Haase <git@dhaase.de>
2017-03-01 22:57:14 +0100

    added more tests for dh.image

--- dh/tests/test_image.py
+++ dh/tests/test_image.py
@@ -2,6 +2,7 @@
 Unit tests for `dh.image`.
 """
 
+import tempfile
 import unittest
 
 import numpy as np
@@ -11,6 +12,29 @@ import dh.image
 
 
 class Test(unittest.TestCase):
+    def test_save_load(self):
+        # images to be saved
+        (_, filename) = tempfile.mkstemp(suffix=".png")
+        I = dh.data.lena()
+        G = dh.image.asgray(I)
+
+        # save, load and check
+        dh.image.save(filename, I)
+        J = dh.image.load(filename, color=True)
+        self.assertEqual(I.shape, J.shape)
+        self.assertTrue(np.all(I == J))
+
+        dh.image.save(filename, G)
+        J = dh.image.load(filename, color=False)
+        self.assertEqual(G.shape, J.shape)
+        self.assertTrue(np.all(G == J))
+
+        dh.image.save(filename, G)
+        J = dh.image.load(filename, color=True)
+        self.assertEqual(I.shape, J.shape)
+        for nChannel in range(3):
+            self.assertTrue(np.all(G == J[:, :, nChannel]))
+
     def test_stack(self):
         # images
         L = dh.data.lena()
@@ -49,6 +73,13 @@ class Test(unittest.TestCase):
         self.assertEqual(S.dtype, np.uint8)
         self.assertAlmostEqual(S.mean(), 89.139465982846545)
 
+    def test_text(self):
+        I = dh.data.lena()
+        dh.image.text(I, "The quick brown fox jumps over the lazy dog.")
+        for nChannel in range(3):
+            self.assertEqual(I[0, 0, nChannel], 0)
+        self.assertAlmostEqual(I.mean(), 121.48126602172852)
+
     def test_convert(self):
         L = dh.data.lena()
 
@@ -57,6 +88,49 @@ class Test(unittest.TestCase):
         self.assertEqual(C.shape, (512, 512, 3))
         self.assertEqual(C.dtype, np.float)
         self.assertAlmostEqual(C.mean(), 0.50285637550104678)
+
+    def test_colorize(self):
+        I = dh.data.lena()
+        C = dh.image.colorize(I, "jet")
+        self.assertEqual(I.shape, C.shape)
+        self.assertEqual(C[128, 256, 1], 174)
+        self.assertEqual(C[256, 128, 2], 64)
+        ms = (180.22365951538086, 99.051216125488281, 105.41025161743164)
+        for nChannel in range(3):
+            mHat = I[:, :, nChannel].mean()
+            self.assertAlmostEqual(mHat, ms[nChannel])
+
+    def test_colorize_all(self):
+        """
+        Colorize slope image with all available colormaps
+        """
+
+        # slope image
+        I = np.arange(256).reshape(1, -1).astype("uint8")
+        self.assertEqual(I.shape, (1, 256))
+
+        # use each colormap...
+        cs = dh.image.colormaps()
+        self.assertIsInstance(cs, dict)
+        self.assertGreaterEqual(len(cs), 75)
+        for (cName, c) in cs.items():
+            # ... to colorize the slope and check the result
+            C = dh.image.colorize(I, c)
+            self.assertEqual(I.shape + (3,), C.shape)
+            for nPixel in range(256):
+                for nChannel in range(3):
+                    self.assertEqual(C[0, nPixel, nChannel], c[nPixel][nChannel])
+
+    def test_gamma(self):
+        I = dh.data.lena()
+        G = dh.image.gamma(I, 0.5)
+        self.assertEqual(I.shape, G.shape)
+        self.assertAlmostEqual(G.mean(), 174.81550725301108)
+
+    def test_resize(self):
+        I = dh.data.lena()
+        R = dh.image.resize(I, 0.5)
+        self.assertEqual(R.shape, (256, 256, 3))
 
     def test_tir(self):
         self.assertEqual(
