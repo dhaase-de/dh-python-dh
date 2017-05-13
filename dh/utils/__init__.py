2724c0647b826477e9ad3017b32d89b2af5dd58c
Daniel Haase <git@dhaase.de>
2017-05-14 00:13:13 +0200

    moved 'dh.utils.ejson' to own module 'dh.ejson' and added support for fractions

--- dh/utils/__init__.py
+++ dh/utils/__init__.py
@@ -15,9 +15,7 @@ import functools
 import hashlib
 import importlib
 import inspect
-import io
 import itertools
-import json
 import math
 import os
 import pprint
@@ -28,13 +26,6 @@ import warnings
 import dh.thirdparty.atomicwrites
 import dh.thirdparty.tabulate
 import dh.thirdparty.tqdm
-
-# NumPy is optional (used in the extended JSON encoder/decoder)
-try:
-    import numpy as np
-    _NUMPY_ERROR = None
-except ImportError as e:
-    _NUMPY_ERROR = e
 
 
 ###
@@ -800,113 +791,6 @@ def pbar(*args, **kwargs):
     """
 
     return dh.thirdparty.tqdm.tqdm(*args, **kwargs)
-
-
-###
-#%% serialization
-###
-
-
-class _ExtendedJsonEncoder(json.JSONEncoder):
-    """
-    JSON encoder which also supports objects of the following types:
-    `bytes`, `numpy.ndarray`.
-
-    For decoding, use `ExtendedJsonDecoder.object_hook` as value for the
-    `object_hook` parameter of `json.load` or `json.loads`.
-    """
-
-    def default(self, o):
-        # byte arrays
-        if isinstance(o, bytes):
-            e = base64.b64encode(o).decode("ascii")
-            return {"__ExtendedJsonType__": "bytes", "__ExtendedJsonValue__": e, "__ExtendedJsonEncoding__": "base64"}
-
-        # NumPy arrays
-        if (_NUMPY_ERROR is None) and isinstance(o, np.ndarray):
-            b = io.BytesIO()
-            np.save(file=b, arr=o, allow_pickle=False, fix_imports=False)
-            e = base64.b64encode(b.getvalue()).decode("ascii")
-            return {"__ExtendedJsonType__": "numpy.ndarray", "__ExtendedJsonValue__": e, "__ExtendedJsonEncoding__": "base64"}
-
-        # no extended object
-        return super().default(o)
-
-
-class _ExtendedJsonDecoder():
-    """
-    This class is the counterpart of `ExtendedJsonEncoder` and provides the
-    static method `object_hook`.
-    """
-
-    @staticmethod
-    def object_hook(o):
-        # only handle dicts which have the three keys "__ExtendedJsonType__", "__ExtendedJsonValue__", and "__ExtendedJsonEncoding__"
-        keys = o.keys()
-        if (len(keys) == 3) and ("__ExtendedJsonType__" in keys) and ("__ExtendedJsonValue__" in keys) and ("__ExtendedJsonEncoding__" in keys) and (o["__ExtendedJsonEncoding__"] == "base64"):
-            # byte arrays
-            if o["__ExtendedJsonType__"] == "bytes":
-                e = o["__ExtendedJsonValue__"]
-                b = base64.b64decode(bytes(e, "ascii"))
-                return b
-
-            # NumPy arrays
-            if o["__ExtendedJsonType__"] == "numpy.ndarray":
-                if _NUMPY_ERROR is None:
-                    e = o["__ExtendedJsonValue__"]
-                    b = base64.b64decode(bytes(e, "ascii"))
-                    x = np.load(file=io.BytesIO(b), allow_pickle=False, fix_imports=False)
-                    return x
-                else:
-                    warnings.warn("Could not decode object of type 'numpy.ndarray', because NumPy import failed: '{}'".format(_NUMPY_ERROR))
-
-        # no extended object
-        return o
-
-
-class ejson():
-    """
-    Class providing static methods `dump()`, `dumps()`, `load()`, `loads()`
-    for JSON serialization and de-serialization just like the `json` module,
-    but with support for more data types - currently, `bytes` and
-    `numpy.ndarray`.
-
-    >>> b = bytes([222, 173, 190, 239])
-    >>> b == ejson.loads(ejson.dumps(b))
-    True
-    """
-
-    @staticmethod
-    def dump(*args, **kwargs):
-        """
-        See :func:`json.dump()`.
-        """
-        return json.dump(*args, **kwargs)
-
-    @staticmethod
-    def dumps(*args, **kwargs):
-        """
-        See :func:`json.dumps()`.
-        """
-        kwargs["ensure_ascii"] = True
-        kwargs["cls"] = _ExtendedJsonEncoder
-        return json.dumps(*args, **kwargs)
-
-    @staticmethod
-    def load(*args, **kwargs):
-        """
-        See :func:`json.load()`.
-        """
-        kwargs["object_hook"] = _ExtendedJsonDecoder.object_hook
-        return json.load(*args, **kwargs)
-
-    @staticmethod
-    def loads(*args, **kwargs):
-        """
-        See :func:`json.loads()`.
-        """
-        kwargs["object_hook"] = _ExtendedJsonDecoder.object_hook
-        return json.loads(*args, **kwargs)
 
 
 ###
