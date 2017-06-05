2479d3191328ad4d9b8bbed68b252b04b417c00d
Daniel Haase <git@dhaase.de>
2017-06-05 23:21:55 +0200

    added colorama to thirdparty modules

--- dh/thirdparty/__init__.py
+++ dh/thirdparty/__init__.py
@@ -7,6 +7,7 @@ non-standard dependencies.
 List of current modules:
 
   * atomicwrites-1.1.5 (https://github.com/untitaker/python-atomicwrites)
+  * colorama-0.3.9 (https://github.com/tartley/colorama)
   * humanize-0.5.1 (https://github.com/jmoiron/humanize)
   * tabulate-0.7.7 (https://bitbucket.org/astanin/python-tabulate)
   * tqdm-4.13.0 (https://github.com/tqdm/tqdm)
