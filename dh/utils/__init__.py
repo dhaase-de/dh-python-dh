91239879fc6c2706631e18a496a6c45c89b1b1e3
Daniel Haase <git@dhaase.de>
2017-06-06 00:45:38 +0200

    added alias for 'dh.thirdparty.transitions.Machine' in 'dh.utils'

--- dh/utils/__init__.py
+++ dh/utils/__init__.py
@@ -27,6 +27,7 @@ import dh.thirdparty.atomicwrites
 import dh.thirdparty.humanize
 import dh.thirdparty.tabulate
 import dh.thirdparty.tqdm
+import dh.thirdparty.transitions
 
 
 ###
@@ -987,6 +988,15 @@ def awopen(*args, **kwargs):
 ###
 #%% general helpers
 ###
+
+
+class Fsm(dh.thirdparty.transitions.Machine):
+    """
+    Finite-state-machine class.
+
+    This class is an alias of :func:`dh.thirdparty.transitions.Machine()`.
+    """
+    pass
 
 
 def rupdate(d, u, deepcopy=True):
