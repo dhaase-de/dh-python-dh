2a19271425e20531b714d70c6e8e22706d942458
Daniel Haase <git@dhaase.de>
2021-12-19 20:16:49 +0100

    Add PyPI upload script

---
+++ scripts/build-upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# robust bash scripting
+set -o errexit
+set -o nounset
+
+# get absolute path of this script
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+source "$SCRIPT_DIR"/setenv.sh
+
+cd "$PACKAGE_DIR" && twine upload dist/dh-0.14.3-py3-none-any.whl --config-file=.pypirc
