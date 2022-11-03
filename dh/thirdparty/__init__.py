dd6281f8a171138e102177ca166ac98c448878bd
Daniel Haase <git@dhaase.de>
2022-11-03 22:00:26 +0100

    Update new thirdparty version numbers in doc

--- dh/thirdparty/__init__.py
+++ dh/thirdparty/__init__.py
@@ -9,8 +9,8 @@ List of current modules:
   * atomicwrites-1.1.5 (https://github.com/untitaker/python-atomicwrites)
   * colorama-0.3.9 (https://github.com/tartley/colorama)
   * humanize-0.5.1 (https://github.com/jmoiron/humanize)
-  * tabulate-0.7.7 (https://bitbucket.org/astanin/python-tabulate)
-  * tqdm-4.13.0 (https://github.com/tqdm/tqdm)
+  * tabulate-0.9.0-14 (https://bitbucket.org/astanin/python-tabulate)
+  * tqdm-4.35.0 (https://github.com/tqdm/tqdm)
 
 Note: `transitions` was modified to remove the requirement for the module
 `six`.
